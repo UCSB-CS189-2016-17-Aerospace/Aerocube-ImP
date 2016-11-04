a89bf6e3e9d81a5e1c59e87b1efcfee2f3786bb4
Andrew Tran <andrew.tran345@gmail.com>
2016-11-04 03:15:01 -0700

    Initial prototype for public interface of image processing

--- imageProcessing/imageProcessingInterfaceTest.py
+++ imageProcessing/imageProcessingInterfaceTest.py
@@ -1,4 +1,11 @@
 import cv2
 import unittest
 
+
 class TestImageProcessingInterfaceMethods(unittest.TestCase):
+
+    def test_load_image(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
