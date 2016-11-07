a25125d0b7631cbb88216661d88b1b86175dc6f2
andrewtran1995 <andrew.tran345@gmail.com>
2016-11-07 00:32:37 -0800

    Added class vars to FiducialMarker; added README.md

--- fiducialMarker/fiducialMarker.py
+++ fiducialMarker/fiducialMarker.py
@@ -1,9 +1,10 @@
 from cv2 import aruco
 
 class FiducialMarker:
+    __dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+    __default_side_pixels = 6
 
     @staticmethod
-    def generateMarker(ID, sidePixels):
-        dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_100)
-        img_marker = aruco.drawMarker(dictionary, ID, sidePixels)
+    def generateMarker(ID, sidePixels=__default_side_pixels):
+        img_marker = aruco.drawMarker(__dictionary, ID, sidePixels)
         return img_marker
