e8228b4e2683dbec0a78adecf3a29f056d29c6e8
andrewtran1995 <andrew.tran345@gmail.com>
2016-11-21 02:53:33 -0800

    Cleaned up cameraCalibration.py and added more documentation

--- imageProcessing/cameraCalibration.py
+++ imageProcessing/cameraCalibration.py
@@ -1,6 +1,6 @@
 import cv2
 from cv2 import aruco
-import numpy as numpy
+import numpy as np
 from collections import namedtuple
 from aerocubeMarker import AeroCubeMarker
 
@@ -12,19 +12,26 @@ class CameraCalibration():
     http://answers.opencv.org/question/98447/camera-calibration-using-charuco-and-python/
     """
     class PredefinedCalibration():
-        _Calibration = namedtuple("_Calibration", "RET_VAL CAMERA_MATRIX DIST_COEFFS")
+        """
+        Inner class to hold different calibration configurations constructed
+        with named tuples. Configurations should be instances of _Calibration
+        with constant-style names (e.g., all upper-case).
+        """
+        _Calibration = namedtuple("_Calibration", "RET_VAL \
+                                                   CAMERA_MATRIX \
+                                                   DIST_COEFFS")
         ANDREW_IPHONE = _Calibration(RET_VAL=3.551523274640683,
-                                     CAMERA_MATRIX=numpy.array([[3.48275636e+03, 0.00000000e+00, 2.02069885e+03],
-                                                                [0.00000000e+00, 3.52274282e+03, 1.51346685e+03],
-                                                                [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]]),
-                                     DIST_COEFFS=numpy.array([[-4.58647345e-02, 1.73122392e+00, -3.30440816e-03, -7.78486275e-04, -7.00795983e+00]]))
-    # (3.551523274640683, array([[  3.48275636e+03,   0.00000000e+00,   2.02069885e+03],
-    #    [  0.00000000e+00,   3.52274282e+03,   1.51346685e+03],
-    #    [  0.00000000e+00,   0.00000000e+00,   1.00000000e+00]]), array([[ -4.58647345e-02,   1.73122392e+00,  -3.30440816e-03,
-    #      -7.78486275e-04,  -7.00795983e+00]]))
+                                     CAMERA_MATRIX=np.array([[3.48275636e+03, 0.00000000e+00, 2.02069885e+03],
+                                                             [0.00000000e+00, 3.52274282e+03, 1.51346685e+03],
+                                                             [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]]),
+                                     DIST_COEFFS=np.array([[-4.58647345e-02, 1.73122392e+00, -3.30440816e-03, -7.78486275e-04, -7.00795983e+00]]))
 
     @staticmethod
     def get_charucoboard():
+        """
+        Create a Charuco (chessboard Aruco) board using pre-set params.
+        :return board: board object created by the aruco call
+        """
         SQUARES_X = 8
         SQUARES_Y = 5
         SQUARE_LENGTH = 10
@@ -37,38 +44,47 @@ class CameraCalibration():
 
     @staticmethod
     def draw_charucoboard(out_size, file_path):
+        """
+        Draw the Charucoboard to file_path
+        :param out_size: size must be tuple (x, y) such that:
+            x >= SQUARES_X * SQUARE_LENGTH
+            y >= SQUARES_Y * SQUARE_LENGTH
+        :param file_path: path to which the board image will be drawn
+        """
         board = CameraCalibration.get_charucoboard()
         print(aruco.drawPlanarBoard(board, out_size))
         cv2.imwrite(file_path, aruco.drawPlanarBoard(board, out_size))
 
     @staticmethod
     def get_calibration_matrices(board, img_arr):
+        """
+        :param board: Charucoboard object to calibrate against
+        :param img_arr: array of images (from different viewpoints)
+        :return ret_val: unknown usage
+        :return camera_matrix: 3X3 camera calibration matrix
+        :return dist_coeffs: camera's distortion coefficients
+        """
         dictionary = AeroCubeMarker.get_dictionary()
         all_charuco_corners = []
         all_charuco_IDs = []
         img_size = None
 
         for img in img_arr:
+            # Convert to grayscale before performing operations
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             corners, IDs, _ = aruco.detectMarkers(gray, dictionary)
             _, charuco_corners, charuco_IDs = aruco.interpolateCornersCharuco(corners, IDs, gray, board)
             all_charuco_corners.append(charuco_corners)
             all_charuco_IDs.append(charuco_IDs)
+            # Get matrix shape of grayscale image
             img_size = gray.shape
-
-        print(all_charuco_corners)
-        print(all_charuco_IDs)
-        print(board)
-        print(img_size)
 
         ret_val, camera_matrix, dist_coeffs, _, _ = aruco.calibrateCameraCharuco(all_charuco_corners, all_charuco_IDs, board, img_size, None, None)
 
         return ret_val, camera_matrix, dist_coeffs
 
 if __name__ == '__main__':
-    # output_path = "./test_files/output_charuco_board_test.png"
-    # output_img_dim = (80*10, 50*10)
-    # CameraCalibration.draw_charucoboard(output_img_dim, output_path)
+    # Get the calibration matrices for ANDREW_IPHONE calibration/configuration
     board = CameraCalibration.get_charucoboard()
     img_paths = ["./test_files/andrew_iphone_calibration_photo_0.jpg",
                  "./test_files/andrew_iphone_calibration_photo_1.jpg",
