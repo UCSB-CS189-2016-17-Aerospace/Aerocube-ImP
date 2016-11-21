8312813c9cca4446bb1d10e10d34daae785787f9
andrewtran1995 <andrew.tran345@gmail.com>
2016-11-21 01:55:55 -0800

    Squashed bug in get_calibration_matrices, and added more calibration photos

--- imageProcessing/cameraCalibration.py
+++ imageProcessing/cameraCalibration.py
@@ -38,14 +38,22 @@ class CameraCalibration():
     def get_calibration_matrices(board, cal_img):
         charuco_corners = None
         charuco_IDs = None
-        board = None
         image_size = None
         # camera_matrix and dist_coeffs can be given None as argument
         camera_matrix = None
         dist_coeffs = None
 
-        charuco_corners, charuco_IDs, _ = aruco.detectMarkers(cal_img, AeroCubeMarker.get_dictionary())
-        image_size = cal_img.shape
+        corners, IDs, _ = aruco.detectMarkers(cal_img, AeroCubeMarker.get_dictionary())
+        # Find the Charuco corners
+        # http://docs.opencv.org/3.1.0/df/d4a/tutorial_charuco_detection.html
+        _, charuco_corners, charuco_IDs = aruco.interpolateCornersCharuco(corners, IDs, cal_img, board)
+        image_size = (cal_img.shape)
+        # image_size = (3024, 4032)
+
+        print(charuco_corners)
+        print(charuco_IDs)
+        print(board)
+        print(image_size)
 
         ret_val, camera_matrix, dist_coeffs, _, _ = aruco.calibrateCameraCharuco(charuco_corners, charuco_IDs, board, image_size, None, None)
 
@@ -56,6 +64,6 @@ if __name__ == '__main__':
     # output_img_dim = (80*10, 50*10)
     # CameraCalibration.draw_charucoboard(output_img_dim, output_path)
     board = CameraCalibration.get_charucoboard()
-    cal_img_path = "./test_files/andrew_iphone_calibration_photo.JPG"
+    cal_img_path = "./test_files/andrew_iphone_calibration_photo_0.jpg"
     cal_img = cv2.imread(cal_img_path)
     print(CameraCalibration.get_calibration_matrices(board, cal_img))
