33e243dcc06a3db9435af9d2a50a679008054a02
andrewtran1995 <andrew.tran345@gmail.com>
2016-12-19 18:25:54 -0800

    Fixed bug in test_identify_aerocubes preventing it from passing

--- imageProcessing/testImageProcessingInterface.py
+++ imageProcessing/testImageProcessingInterface.py
@@ -66,7 +66,7 @@ class TestImageProcessingInterfaceMethods(unittest.TestCase):
         aerocube_face = AeroCubeFace.ZENITH
         corners = self.VALID_CORNER_MAT
         marker_list = [AeroCubeMarker(aerocube_ID, aerocube_face, corners)]
-        aerocube_list = AeroCube(marker_list)
+        aerocube_list = [AeroCube(marker_list)]
         imp = ImageProcessor(self.test_img_path)
         self.assertEqual(imp._identify_aerocubes(), aerocube_list)
 
