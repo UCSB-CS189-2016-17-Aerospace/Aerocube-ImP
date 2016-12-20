2c211b9e5c6a5f33784f0e707a794e8a379fb89f
andrewtran1995 <andrew.tran345@gmail.com>
2016-12-19 21:35:00 -0800

    Restructured format of constants and expected values in test file

--- imageProcessing/testImageProcessingInterface.py
+++ imageProcessing/testImageProcessingInterface.py
@@ -3,6 +3,7 @@ import cv2
 from cv2 import aruco
 import numpy as np
 import os
+from collections import namedtuple
 from .aerocubeMarker import AeroCubeMarker, AeroCubeFace, AeroCube
 from .imageProcessingInterface import ImageProcessor
 from .settings import ImageProcessingSettings
@@ -15,9 +16,23 @@ class TestImageProcessingInterfaceMethods(unittest.TestCase):
                                   [454., 417.],
                                   [82.,  417.]]])
     test_files_path = ImageProcessingSettings.get_test_files_path()
+    # create named tuple to help organize test files and expected results of scan
+    TestFile = namedtuple('TestFile', 'img_path \
+                                       corners \
+                                       IDs')
     test_img_path = os.path.join(test_files_path, 'marker_4X4_sp6_id0.png')
     test_output_path = os.path.join(test_files_path, 'output.png')
-    test_img_mult_aerocubes_path = os.path.join(test_files_path, '0_ZENITH_1_ZENITH.jpg')
+    # struct for image 2_ZENITH_0_BACK.jpg
+    TEST_MULT_AEROCUBES = TestFile(img_path=os.path.join(test_files_path, '2_ZENITH_0_BACK.jpg'),
+                                   corners=[np.array([[[371.,  446.],
+                                                       [396.,  505.],
+                                                       [312.,  533.],
+                                                       [290.,  471.]]]),
+                                            np.array([[[801.,  314.],
+                                                       [842.,  359.],
+                                                       [779.,  380.],
+                                                       [741.,  333.]]])],
+                                   IDs=np.array([12, 4]))
 
     def test_init(self):
         imp = ImageProcessor(self.test_img_path)
@@ -45,6 +60,18 @@ class TestImageProcessingInterfaceMethods(unittest.TestCase):
         output_img = aruco.drawDetectedMarkers(imp._img_mat, test_corners, test_ids)
         cv2.imwrite(self.test_output_path, output_img)
 
+    def test_find_fiducial_marker_multiple(self):
+        # get results from ImP
+        imp = ImageProcessor(self.TEST_MULT_AEROCUBES.img_path)
+        test_corners, test_ids = imp._find_fiducial_markers()
+        # assert hard-coded results equal ImP results
+        self.assertTrue(np.array_equal(self.TEST_MULT_AEROCUBES.corners, test_corners))
+        self.assertTrue(np.array_equal(self.TEST_MULT_AEROCUBES.IDs, test_ids))
+
+    @unittest.skip("Not implemented")
+    def test_find_fiducial_marker_none(self):
+        self.fail()
+
     def test_find_aerocube_marker(self):
         # hard code results of operation
         aerocube_ID = 0
@@ -60,8 +87,14 @@ class TestImageProcessingInterfaceMethods(unittest.TestCase):
         self.assertTrue(np.array_equal(true_markers, aerocube_markers))
 
     @unittest.skip("Not implemented")
-    def test_find_multiple_aerocube_markers(self):
+    def test_find_aerocube_markers_multiple(self):
         self.assertTrue(False)
+        imp = ImageProcessor(self.test_img_mult_aerocubes_path)
+        aerocube_markers = imp._find_aerocube_markers()
+
+    @unittest.skip("Not implemented")
+    def test_find_aerocube_markers_none(self):
+        self.fail()
 
     def test_identify_aerocubes(self):
         aerocube_ID = 0
@@ -74,13 +107,13 @@ class TestImageProcessingInterfaceMethods(unittest.TestCase):
 
     @unittest.skip("Not implemented")
     def test_identify_multiple_aerocubes(self):
-        self.assertTrue(False)
+        self.fail()
 
     def test_scan_image(self):
         imp = ImageProcessor(self.test_img_path)
         scan_results = imp.scan_image(ImageEventSignal.IDENTIFY_AEROCUBES)
         # TODO: force this to fail until _identify_aerocubes is implemented
-        self.assertTrue(False)
+        self.fail()
 
     def test_draw_fiducial_markers(self):
         imp = ImageProcessor(self.test_img_path)
